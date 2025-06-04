b9aee0d6aef9a5d8fde1dabbf44e83a68cc918fe
CoinVest AI Innovations <coinvest518@gmail.com>
2025-06-04 16:14:16 -0400

    Delete key-to-array.js

--- key-to-array.js
+++
@@ -1,9 +0,0 @@
-const bs58 = require('bs58');
-const fs = require('fs');
-
-// Replace with your base58 private key string
-const base58 = '3RemjGHExAUzYkjZS4ySxXtuK8MtUJWHZEcJbsyiXir88Cq4K1Ch8CtRfrj1HHcdTYCmswu8FDY7aw8sPqpeTBtX';
-
-const arr = Array.from(bs58.decode(base58));
-fs.writeFileSync('private-key-array.json', JSON.stringify(arr));
-console.log('Done! Array written to private-key-array.json');
